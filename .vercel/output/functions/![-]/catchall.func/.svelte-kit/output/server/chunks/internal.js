91befaaf2dc1f774ca5da6a61972de9fafabe936
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-10-20 10:30:49 -0700

    Remove build overrides -  vercel.json and scripts that touched .vercel/output

--- .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/![-]/catchall.func/.svelte-kit/output/server/chunks/internal.js
@@ -962,7 +962,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "9e6lyv"
+  version_hash: "quv845"
 };
 async function get_hooks() {
   let handle;
